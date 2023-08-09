60b28702c9da23eb6f04e79cc40e6ac1e549fc4b
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-08-09 22:14:27 +0700

    feat(`admin.py`): for aggregating models

--- todo/tasks/admin.py
+++ todo/tasks/admin.py
@@ -2,3 +2,8 @@ from django.contrib import admin
 
 from tasks.models import User, Task, Zone, TaskSettings
 
+
+admin.site.register(User)
+admin.site.register(Task)
+admin.site.register(Zone)
+admin.site.register(TaskSettings)
