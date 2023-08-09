685e762d383c5a81b4fe779c7a20cec71411186e
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-08-09 22:13:05 +0700

    feat(`settings.py`): invisibility for `SECRET_KEY`

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -1,9 +1,14 @@
 from os import getenv
 
 from pathlib import Path
+from dotenv import load_dotenv
 
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = getenv('SECRET_KEY')
 
 DEBUG = True
 
