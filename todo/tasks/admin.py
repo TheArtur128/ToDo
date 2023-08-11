2a8d9c0b819e8e4eee669510adcdf168f35ae6e7
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-08-11 21:42:10 +0700

    feat(`admin.py`): `Position` administration

--- todo/tasks/admin.py
+++ todo/tasks/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from tasks.models import User, Task, Zone, TaskSettings
+from tasks.models import User, Task, Zone, TaskSettings, Position
 
 
 admin.site.register(User)
 admin.site.register(Task)
 admin.site.register(Zone)
 admin.site.register(TaskSettings)
+admin.site.register(Position)
