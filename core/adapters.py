569e08fe06efa675f228cb127d106447a768b17b
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-08-28 17:10:00 +0700

    feat(`core`): cache repository

---
+++ core/adapters.py
@@ -0,0 +1,31 @@
+from typing import Generic
+
+from act import K, V
+from django.core.cache import caches
+
+
+__all__ = ("ChachRepository", )
+
+
+class ChachRepository(Generic[K, V]):
+    def __init__(self, subject: str, *, salt: str, location: str) -> None:
+        self.__subject = subject
+        self.__salt = salt
+        self.__location = location
+
+    def __repr__(self) -> str:
+        return "{}[{}]({}:{})".format(
+            type(self).__name__, self.__location, self.__salt, self.__subject
+        )
+
+    def __getitem__(self, key: K) -> V:
+        return caches[self.__location].get(self.__key_for(key))
+
+    def __setitem__(self, key: K, value: V) -> None:
+        caches[self.__location].set(self.__key_for(key), value)
+
+    def __delitem__(self, key: key) -> bool:
+        return caches[self.__location].delete(key)
+
+    def __key_for(self, *, key: key) -> str:
+        return f"{self.__salt}:{self.__subject}:{key}"
