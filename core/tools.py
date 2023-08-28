6df74258eb5aab9849fd37f4cafc5d2443e6d652
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-08-28 17:06:46 +0700

    feat(`tools.py`): `obj` enum

--- core/tools.py
+++ core/tools.py
@@ -6,3 +6,6 @@ from act import ok, bad, temp, obj
 def status_of(condition: bool) -> ok[None] | bad[None]:
     return ok(None) if condition else bad(None)
 
+
+def name_enum_of(annotated: temp(__annotations__=Iterable[str])) -> obj:
+    return obj.of({name: name for name in annotated.__annotations__.keys()})
