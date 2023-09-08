ad102fea5c2ec7d957de59ea4947444b6491cd90
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-09-08 23:14:27 +0700

    feat(`core.tools`): functor from `optionally` to `maybe`

--- core/tools.py
+++ core/tools.py
@@ -1,10 +1,14 @@
 from typing import Iterable
 
-from act import temp, obj
+from act import temp, obj, Special, bad, V
 
 
-__all__ = ("name_enum_of", )
+__all__ = ("name_enum_of", "bad_or")
 
 
 def name_enum_of(annotated: temp(__annotations__=Iterable[str])) -> obj:
     return obj.of({name: name for name in annotated.__annotations__.keys()})
+
+
+def bad_or(value: Special[None, V]) -> V | bad[None]:
+    return bad(None) if value is None else value
