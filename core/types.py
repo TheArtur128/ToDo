e9de13802d416627e4ebd6eb2d0b9a9266cb6955
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-09-08 23:13:43 +0700

    feat(`core.types`): for error messages and names

--- core/types.py
+++ core/types.py
@@ -1,10 +1,14 @@
 from typing import TypeAlias
 
 
-__all__ = ('URL', "Email", "Password", "PasswordHash")
+__all__ = ("ErrorMessage", 'URL', "Email", "Password", "PasswordHash", "Name")
 
+
+ErrorMessage = str
 
 URL: TypeAlias = str
 Email: TypeAlias = str
 Password: TypeAlias = str
 PasswordHash: TypeAlias = str
+
+Name: TypeAlias = str
