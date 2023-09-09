89701be1ea97041fabd0dcd181a550922c800097
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-09-09 20:33:09 +0700

    feat(`access.services`): `open_registration_port` stub

---
+++ access/services.py
@@ -0,0 +1,11 @@
+from typing import Optional
+
+from core.types import Name, Email, Password, URL
+
+
+def open_registration_port(  # todo: implement
+    name: Name,
+    email: Email,
+    password: Password,
+) -> Optional[URL]:
+    ...
