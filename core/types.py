eebf946dfb82c9839d8436dc0d5ba49acc7a74e8
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-09-09 20:34:49 +0700

    feat(`core.types`): annotation type

--- core/types.py
+++ core/types.py
@@ -1,10 +1,14 @@
 from typing import TypeAlias
 
 
-__all__ = ("ErrorMessage", 'URL', "Email", "Password", "PasswordHash", "Name")
+__all__ = (
+    "ErrorMessage", "Annotaton", 'URL', "Email", "Password", "PasswordHash",
+    "Name"
+)
 
 
 ErrorMessage: TypeAlias = str
+Annotaton: TypeAlias = Any
 
 URL: TypeAlias = str
 Email: TypeAlias = str
