d0c43defba4f6f15ea6a5c3b0bec976f4034ca10
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-09-11 16:15:01 +0700

    fix(`open_email_port_of`): actual names

--- access/confirmation/facade.py
+++ access/confirmation/facade.py
@@ -2,5 +2,5 @@ def open_email_port_of(subject: Subject, *, for_: Email) -> Optional[URL]:
     return _open_port_of(
         subject,
         for_=contextual(id_groups.email, for_),
-        notify=_send_confirmation_mail_to,
+        notify=send_confirmation_mail_to,
     )
