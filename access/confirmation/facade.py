f0aa79d9aec1e28e2f8ccada262a8f0d301720a2
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-09-11 16:16:21 +0700

    feat(`access.confirmation.facade`): repository template

--- access/confirmation/facade.py
+++ access/confirmation/facade.py
@@ -4,3 +4,16 @@ def open_email_port_of(subject: Subject, *, for_: Email) -> Optional[URL]:
         for_=contextual(id_groups.email, for_),
         notify=send_confirmation_mail_to,
     )
+
+
+ViewHandler: TypeAlias = Callable[[AuthToken, HttpRequest, I], Optional[HttpResponse]]
+
+
+@closing(close_port_of=close_port_of)
+@obj.of
+class HandlerChachRepository(Generic[I]):
+    def get_of(port_id: PortID) -> ViewHandler:
+        ...
+
+    def registrate_for(port_id: PortID) -> reformer_of[ViewHandler]:
+        ...
