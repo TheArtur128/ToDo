c780c625876e4f6fc6f25d1c82d4980876914a5f
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-09-15 21:54:32 +0700

    ref(`access.confirmation.facade`): actualization

--- access/confirmation/facade.py
+++ access/confirmation/facade.py
@@ -7,22 +7,55 @@ from access.confirmation.adapters import send_confirmation_mail_to
 from access.confirmation.core import Subject
 
 
-def open_email_port_of(subject: Subject, *, for_: Email) -> Optional[URL]:
-    return _open_port_of(
-        subject,
-        for_=contextual(id_groups.email, for_),
-        notify=send_confirmation_mail_to,
+HANDLER_REPOSITORY = ConfigHandlerRepository(django_config_repository)
+
+
+def activate_by(
+    access: PortAccess,
+    request: HttpRequest,
+) -> Optional[HttpRequest]:
+    return activate_by(
+        access,
+        password_hash_of=_._password_hashes_of(a)[b],
+        hash_equals=check_password,
+        id_of=_._ids_that(a)[b],
+        payload_of=HANDLER_REPOSITORY.get_of |then>> rwill(partial)(request),
+        port_closing_by=will(for_effect)(close_port_of),
     )
 
 
-ViewHandler: TypeAlias = Callable[[AuthToken, HttpRequest, I], Optional[HttpResponse]]
+def open_port_of(
+    subject: core.Subject,
+    *,
+    for_: str,
+    method: core.IdGroup,
+) -> Optional[URL]:
+    generate_auth_token = token_urlsafe |to| settings.PORT_AUTH_TOKEN_LENGTH
+    generate_password = token_urlsafe |to| settings.PORT_PASSWORD_LENGTH
+
+    return core.open_port_of(
+        port_id=PortID(subject=subject, id_group=method),
+        for_=for_,
+        generate_auth_token=generate_auth_token,
+        generate_password=generate_password,
+        password_hash_of=make_password,
+        access_token_of=confirmation_page_url_of,
+        notify_by=send_confirmation_mail_to,
+        create_port_from=create_port,
+    )
 
 
-@closing(close_port_of=close_port_of)
-@obj.of
-class HandlerChachRepository(Generic[I]):
-    def get_of(port_id: PortID) -> ViewHandler:
-        ...
+@name_enum_of
+class id_groups:
+    email: IdGroup
 
-    def registrate_for(port_id: PortID) -> reformer_of[ViewHandler]:
-        ...
+
+@name_enum_of
+class subjects:
+    authorization: Subject
+    registration: Subject
+
+    @name_enum_of
+    class access_recovery:
+        via_email: Subject
+        via_name: Subject
