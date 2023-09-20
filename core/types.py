1ebac454366b7d6445293a62b5bbfa2ae51d45a4
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-09-20 21:35:35 +0700

    feat(`core.types`): `TypeAlias` for `ID`

--- core/types.py
+++ core/types.py
@@ -6,6 +6,7 @@ ErrorMessage: TypeAlias = str
 Annotaton: TypeAlias = Any
 
 URL: TypeAlias = str
+ID: TypeAlias = str
 Email: TypeAlias = str
 Password: TypeAlias = str
 PasswordHash: TypeAlias = str
