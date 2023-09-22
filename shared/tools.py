7f5d3a5f2ec09280421682994677a29bbd0abf63
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-09-23 01:38:20 +0700

    feat(`shared.tools`): factory for `temp`

--- shared/tools.py
+++ shared/tools.py
@@ -1,6 +1,7 @@
-from typing import Iterable
+from dataclasses import dataclass
+from typing import Iterable, TypeVar
 
-from act import temp, obj, Special, bad, V
+from act import temp, obj, Special, bad, Unia, V
 
 
 def name_enum_of(annotated: temp(__annotations__=Iterable[str])) -> obj:
@@ -9,3 +10,10 @@ def name_enum_of(annotated: temp(__annotations__=Iterable[str])) -> obj:
 
 def bad_or(value: Special[None, V]) -> V | bad[None]:
     return bad(None) if value is None else value
+
+
+def struct(type_: type) -> Unia[temp, temp(T=TypeVar)]:
+    template = temp.of(dataclass(type_))
+    with_type_var = obj(T=TypeVar(f"{type_.__name__}T", bound=template))
+
+    return template & with_type_var
