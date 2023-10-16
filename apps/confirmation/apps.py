0e2e3e58e8b57a1e2644c7bcc207c23f2c93ec98
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-10-16 21:55:20 +0700

    feat(`apps`): `label` for configs

--- apps/confirmation/apps.py
+++ apps/confirmation/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ConfirmationConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.confirmation"
+    label = "confirmation"
