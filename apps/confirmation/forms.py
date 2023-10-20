9d4a8cedb531786b1037a701d6d865235f46a479
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-10-20 23:05:12 +0700

    feat(`ConfirmationForm`): informative errors

--- apps/confirmation/forms.py
+++ apps/confirmation/forms.py
@@ -3,7 +3,17 @@ from django.forms import Form, CharField
 
 
 class ConfirmationForm(Form):
+    __error_message = "Password must be {} characters long.".format(
+        settings.CONFIRMATION_ACTIVATION_CODE_LENGTH,
+    )
+
+    __error_messages = dict(
+        max_length=__error_message,
+        min_length=__error_message,
+    )
+
     password = CharField(
         min_length=settings.CONFIRMATION_ACTIVATION_CODE_LENGTH,
         max_length=settings.CONFIRMATION_ACTIVATION_CODE_LENGTH,
+        error_messages=__error_messages,
     )
