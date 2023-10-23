d74c7ce6e17227b957a5eb4a10bc5b5d15a57e33
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-10-23 16:50:05 +0700

    fix(`token_generator_with`): for specified length

--- apps/shared/tools.py
+++ apps/shared/tools.py
@@ -1,12 +1,19 @@
 from secrets import token_urlsafe
 from typing import Callable
 
-from act import to
+from act import fun, then, to
+from act.cursors.static import t
 
 from apps.shared.types_ import Token
 
 
 def token_generator_with(*, length: int) -> Callable[[], Token]:
+    if (length - 1) % 4 == 0:
+        generate = token_generator_with(length=length + 1)
+        cut = fun(t[:length])
+
+        return fun(generate |then>> cut)
+
     byte_number = int(length * 3 / 4)
 
     return token_urlsafe |to| byte_number
