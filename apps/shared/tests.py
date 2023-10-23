d4ef2aa5190fe8f182132970e9606e1c03faacc2
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-10-23 16:48:48 +0700

    tests(`tools`): for `token_generator_with`

--- apps/shared/tests.py
+++ apps/shared/tests.py
@@ -1,8 +1,17 @@
 from pytest import mark
 
-from apps.shared import hashing
+from apps.shared import hashing, tools
 
 
 @mark.parametrize("text", ["some mega secrect", str()])
 def test_hashing_isomorphism(text: str) -> None:
     assert hashing.unhashed(hashing.hashed(text)) == text
+
+
+@mark.parametrize("token_length", [*range(10), 32, 64, 128, 1024, 2069])
+def test_token_generator_with(token_length: int) -> None:
+    generate = tools.token_generator_with(length=token_length)
+
+    assert len(generate()) == token_length
+    assert len(generate()) == token_length
+    assert len(generate()) == token_length
