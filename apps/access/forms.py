7670a11d26a493fa63b0fe60c8ce855321d16efe
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-11-15 01:41:22 +0700

    feat(`access` front): add a short password error message

--- apps/access/forms.py
+++ apps/access/forms.py
@@ -1,28 +1,41 @@
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import forms
 
 from apps.access.models import User
 
 
-class UserLoginForm(AuthenticationForm):
+class UserLoginForm(forms.AuthenticationForm):
     class Meta:
         model = User
         fields = ("name", "password")
 
 
-class UserRegistrationForm(UserCreationForm):
+class _WithPasswordErrorMessageMixin:
+    error_messages = {"password_mismatch": "Password mismatch"}
+
+
+class UserRegistrationForm(
+    _WithPasswordErrorMessageMixin,
+    forms.UserCreationForm,
+):
     class Meta:
         model = User
         fields = ("name", "email", "password1", "password2")
 
 
-class RestoringAccessByNameForm(UserCreationForm):
+class RestoringAccessByNameForm(
+    _WithPasswordErrorMessageMixin, 
+    forms.BaseUserCreationForm,
+):
     class Meta:
         model = User
         fields = ("name", "password1", "password2")
         validate_unique = False
 
 
-class RestoringAccessByEmailForm(UserCreationForm):
+class RestoringAccessByEmailForm(
+    _WithPasswordErrorMessageMixin,
+    forms.BaseUserCreationForm,
+):
     class Meta:
         model = User
         fields = ("email", "password1", "password2")
