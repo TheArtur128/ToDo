8d565bd1ba43f758f20e2e52b87a579950a563a4
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-11-15 20:26:55 +0700

    fix(`forms`): to validate

--- apps/access/forms.py
+++ apps/access/forms.py
@@ -1,6 +1,32 @@
 from django.contrib.auth import forms
+from django.forms import (
+    Form, ValidationError, PasswordInput, CharField, EmailField
+)
 
 from apps.access.models import User
+from apps.access.types_ import Password
+
+
+_password_error_messages = dict(password_mismatch="Password mismatch")
+
+
+class _PasswordConfirmationForm(Form):
+    error_messages = _password_error_messages
+
+    password1 = CharField(widget=PasswordInput, max_length=128)
+    password2 = CharField(widget=PasswordInput, max_length=128)
+
+    def clean_password2(self) -> Password:
+        password1 = self.cleaned_data["password1"]
+        password2 = self.cleaned_data["password2"]
+
+        if password1 != password2:
+            raise ValidationError(
+                self.error_messages["password_mismatch"],
+                code="password_mismatch",
+            )
+
+        return password2
 
 
 class UserLoginForm(forms.AuthenticationForm):
@@ -9,34 +35,17 @@ class UserLoginForm(forms.AuthenticationForm):
         fields = ("name", "password")
 
 
-class _WithPasswordErrorMessageMixin:
-    error_messages = {"password_mismatch": "Password mismatch"}
+class UserRegistrationForm(forms.UserCreationForm):
+    error_messages = _password_error_messages
 
-
-class UserRegistrationForm(
-    _WithPasswordErrorMessageMixin,
-    forms.UserCreationForm,
-):
     class Meta:
         model = User
         fields = ("name", "email", "password1", "password2")
 
 
-class RestoringAccessByNameForm(
-    _WithPasswordErrorMessageMixin, 
-    forms.BaseUserCreationForm,
-):
-    class Meta:
-        model = User
-        fields = ("name", "password1", "password2")
-        validate_unique = False
+class RestoringAccessByNameForm(_PasswordConfirmationForm):
+    name = CharField(max_length=128)
 
 
-class RestoringAccessByEmailForm(
-    _WithPasswordErrorMessageMixin,
-    forms.BaseUserCreationForm,
-):
-    class Meta:
-        model = User
-        fields = ("email", "password1", "password2")
-        validate_unique = False
+class RestoringAccessByEmailForm(_PasswordConfirmationForm):
+    email = EmailField(max_length=154)
