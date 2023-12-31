41a2a80381f9c8b89d93592931c59c524477790a
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-12-31 17:58:39 +0700

    ref(`urls`): use module import

--- apps/access/urls.py
+++ apps/access/urls.py
@@ -1,24 +1,22 @@
 from django.urls import path
 
-from apps.access.views import (
-    login, registrate, logout, restore_access_by_name, restore_access_by_email
-)
+from apps.access import views
 
 
 app_name = "access"
 
 urlpatterns = [
-    path("sign-in", login, name="sign-in"),
-    path("sign-up", registrate, name="sign-up"),
-    path("logout", logout, name="logout"),
+    path("sign-in", views.login, name="sign-in"),
+    path("sign-up", views.registrate, name="sign-up"),
+    path("logout", views.logout, name="logout"),
     path(
         "restore-access-by-name",
-        restore_access_by_name,
+        views.restore_access_by_name,
         name="restore-by-name"
     ),
     path(
         "restore-access-by-email",
-        restore_access_by_email,
+        views.restore_access_by_email,
         name="restore-by-email"
     ),
 ]
