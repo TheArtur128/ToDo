541921aeb28eb3de51864b31cf789a4713daaf1d
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2024-02-29 12:47:32 +0700

    style(`models`): use namespaces

--- apps/shared/models.py
+++ apps/shared/models.py
@@ -1,15 +1,5 @@
-from django.contrib.auth.models import (
-    AbstractBaseUser, BaseUserManager, PermissionsMixin
-)
-from django.contrib.postgres.indexes import HashIndex
-from django.db.models import (
-    Model, CASCADE, PositiveIntegerField, DateTimeField, BooleanField,
-    CharField, EmailField, ManyToManyField, ForeignKey, PROTECT,
-    OneToOneField, IntegerChoices, IntegerField, DurationField, SET_NULL
-)
-
-
-__all__ = ("TaskSettings", "Zone", "Task", "User")
+from django.contrib import auth, postgres
+from django.db import models
 
 
 class _VisualizableMixin:
@@ -17,15 +7,19 @@ class _VisualizableMixin:
         return f"{type(self).__name__}({self})"
 
 
-class Space(_VisualizableMixin, Model):
-    name = CharField(max_length=64, default=None, null=True, blank=True)
-    user = ForeignKey("User", on_delete=CASCADE, related_name="spaces")
-    user_x = PositiveIntegerField()
-    user_y = PositiveIntegerField()
+class Space(_VisualizableMixin, models.Model):
+    name = models.CharField(max_length=64, default=None, null=True, blank=True)
+    user = models.ForeignKey(
+        "User",
+        on_delete=models.CASCADE,
+        related_name="spaces",
+    )
+    user_x = models.PositiveIntegerField()
+    user_y = models.PositiveIntegerField()
 
-    next = OneToOneField(
+    next = models.OneToOneField(
         "self",
-        on_delete=CASCADE,
+        on_delete=models.CASCADE,
         default=None,
         null=True,
         blank=True,
@@ -35,22 +29,22 @@ class Space(_VisualizableMixin, Model):
         return f"{self.id}: {self.name}"
 
 
-class _Positionable(Model):
+class _Positionable(models.Model):
+    space = models.ForeignKey(Space, on_delete=models.CASCADE)
+    x = models.PositiveIntegerField()
+    y = models.PositiveIntegerField()
+
     class Meta:
         abstract = True
 
-    space = ForeignKey(Space, on_delete=CASCADE)
-    x = PositiveIntegerField()
-    y = PositiveIntegerField()
-
 
 class Task(_VisualizableMixin, _Positionable):
-    class Status(IntegerChoices):
+    class Status(models.IntegerChoices):
         active = (1, "active")
         done = (2, "done")
         failed = (3, "failed")
 
-    class Color(IntegerChoices):
+    class Color(models.IntegerChoices):
         first = (1, "first")
         second = (2, "second")
         third = (3, "third")
@@ -58,22 +52,22 @@ class Task(_VisualizableMixin, _Positionable):
         fifth = (5, "fifth")
         sixth = (6, "sixth")
 
-    description = CharField(max_length=128)
-    status = IntegerField(choices=Status.choices, default=Status.active)
-    settings = OneToOneField(
+    description = models.CharField(max_length=128)
+    status = models.IntegerField(choices=Status.choices, default=Status.active)
+    settings = models.OneToOneField(
         "TaskSettings",
-        on_delete=SET_NULL,
+        on_delete=models.SET_NULL,
         default=None,
         null=True,
         blank=True,
     )
 
-    color = IntegerField(choices=Color.choices)
-    is_hidden = BooleanField(default=False)
+    color = models.IntegerField(choices=Color.choices)
+    is_hidden = models.BooleanField(default=False)
 
-    subspace = OneToOneField(
+    subspace = models.OneToOneField(
         Space,
-        on_delete=SET_NULL,
+        on_delete=models.SET_NULL,
         related_name="root",
         default=None,
         null=True,
@@ -84,8 +78,8 @@ class Task(_VisualizableMixin, _Positionable):
         return f"{self.id}: {self.description}"
 
 
-class TaskSettings(_VisualizableMixin, Model):
-    remove_on = IntegerField(
+class TaskSettings(_VisualizableMixin, models.Model):
+    remove_on = models.IntegerField(
         choices=Task.Status.choices,
         default=Task.Status.done,
         null=True,
@@ -95,29 +89,29 @@ class TaskSettings(_VisualizableMixin, Model):
         return str(self.id)
 
 
-class TaskEvent(_VisualizableMixin, Model):
-    settings = ForeignKey(
-        TaskSettings, on_delete=CASCADE, related_name="events"
+class TaskEvent(_VisualizableMixin, models.Model):
+    settings = models.ForeignKey(
+        TaskSettings, on_delete=models.CASCADE, related_name="events"
     )
 
-    activation_time = DateTimeField(default=None, null=True, blank=True)
-    repetition_time = DurationField(default=None, null=True, blank=True)
+    activation_time = models.DateTimeField(default=None, null=True, blank=True)
+    repetition_time = models.DurationField(default=None, null=True, blank=True)
 
-    new_status = IntegerField(
+    new_status = models.IntegerField(
         choices=Task.Status.choices,
         default=None,
         null=True,
     )
-    new_is_hidden = BooleanField(default=None, null=True, blank=True)
+    new_is_hidden = models.BooleanField(default=None, null=True, blank=True)
 
-    is_deleter = BooleanField(default=False, blank=True)
+    is_deleter = models.BooleanField(default=False, blank=True)
 
-    x_vector = IntegerField(default=0, blank=True)
-    y_vector = IntegerField(default=0, blank=True)
+    x_vector = models.IntegerField(default=0, blank=True)
+    y_vector = models.IntegerField(default=0, blank=True)
 
-    task_to_add = OneToOneField(
+    task_to_add = models.OneToOneField(
         Task,
-        on_delete=SET_NULL,
+        on_delete=models.SET_NULL,
         default=None,
         null=True,
         blank=True,
@@ -125,12 +119,12 @@ class TaskEvent(_VisualizableMixin, Model):
 
 
 class Zone(_VisualizableMixin, _Positionable):
-    width = PositiveIntegerField()
-    height = PositiveIntegerField()
-    tasks = ManyToManyField("Task", related_name="zones")
-    settings = OneToOneField(
+    width = models.PositiveIntegerField()
+    height = models.PositiveIntegerField()
+    tasks = models.ManyToManyField("Task", related_name="zones")
+    settings = models.OneToOneField(
         TaskSettings,
-        on_delete=SET_NULL,
+        on_delete=models.SET_NULL,
         default=None,
         null=True,
         blank=True,
@@ -140,7 +134,7 @@ class Zone(_VisualizableMixin, _Positionable):
         return str(self.id)
 
 
-class CustomUserManager(BaseUserManager):
+class CustomUserManager(auth.models.BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError("Email field is required")
@@ -159,16 +153,20 @@ class CustomUserManager(BaseUserManager):
         return self.create_user(email, password, **extra_fields)
 
 
-class User(_VisualizableMixin, AbstractBaseUser, PermissionsMixin):
-    name = CharField(max_length=128, unique=True)
-    email = EmailField(max_length=154, unique=True)
-    password = CharField(max_length=128)
-    is_active = BooleanField(default=True)
-    is_staff = BooleanField(default=False)
-    is_superuser = BooleanField(default=False)
+class User(
+    _VisualizableMixin,
+    auth.models.AbstractBaseUser,
+    auth.models.PermissionsMixin,
+):
+    name = models.CharField(max_length=128, unique=True)
+    email = models.EmailField(max_length=154, unique=True)
+    password = models.CharField(max_length=128)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
-    tasks = ManyToManyField(Task, blank=True)
-    default_settings = OneToOneField(TaskSettings, on_delete=PROTECT)
+    tasks = models.ManyToManyField(Task, blank=True)
+    default_settings = models.OneToOneField(TaskSettings, on_delete=models.PROTECT)
 
     USERNAME_FIELD = "name"
     EMAIL_FIELD = "email"
@@ -177,7 +175,10 @@ class User(_VisualizableMixin, AbstractBaseUser, PermissionsMixin):
     objects = CustomUserManager()
 
     class Meta:
-        indexes = [HashIndex(fields=["name"]), HashIndex(fields=["email"])]
+        indexes = [
+            postgres.indexes.HashIndex(fields=["name"]),
+            postgres.indexes.HashIndex(fields=["email"]),
+        ]
 
     def has_module_perms(self, label) -> bool:
         return self.is_superuser and self.is_staff and self.is_active
