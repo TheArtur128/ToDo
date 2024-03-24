dd34b2f645f62bcd76e36287ec8325282f113ff8
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2024-03-24 23:49:38 +0700

    chore(`map`): add mappers for `models` (#8)

--- apps/map/adapters/repos.py
+++ apps/map/adapters/repos.py
@@ -1,7 +1,6 @@
-from typing import Optional, Any
+from typing import Optional
 
-from act import optionally, val, obj, original_of, type
-from django.db import transaction
+from act import optionally, val
 from rest_framework.request import Request
 
 from apps.map import models
@@ -13,60 +12,27 @@ type User = models.User
 type Task = models.Task
 
 
-@obj
+@val
 class django_orm_users:
-    def user_of(self, id: int) -> Optional[User]:
+    def user_of(id: int) -> Optional[User]:
         user_record = models.User.objects.filter(id=id).first()
 
-        return optionally(sculptures.user_of)(user_record)
+        return optionally(sculptures.rules.user_of)(user_record)
 
-    def saved(self, user: rules.User) -> User:
-        settings_record = self._create_task_settings(user.global_task_settings)
+    def saved(user: rules.User) -> rules.User:
+        user_record = sculptures.DjangoOrmRecords(are_saved=True).user_of(user)
 
-        user_record = models.User.objects.create(
-            id=user.id,
-            global_task_settings=settings_record,
-        )
-
-        for top_map in user.maps:
-            self._create_map(top_map, user_record)
-
-        return sculptures.user_of(user_record)
-
-    def _create_map(
-        self,
-        top_map: rules.TopMap,
-        user_record: models.User,
-    ) -> models.MapTop:
-        map_record = models.Map.objects.create(
-            x=top_map.user_position.x,
-            y=top_map.user_position.y,
-        )
-
-        return models.MapTop.objects.create(
-            id=top_map.id,
-            map=map_record,
-            user=user_record,
-            name=top_map.name,
-        )
-
-    @staticmethod
-    @optionally
-    def _create_task_settings(
-        settings_record: rules.TaskSettings,
-    ) -> models.TaskSettings:
-        return models.TaskSettings.objects.create(
-            remove_task_on=settings_record.remove_task_on.value
-        )
+        return sculptures.rules.user_of(user_record)
 
 
+@val
 class django_orm_case_tasks:
     class Users:
         def __init__(self, request: Request) -> None:
             self.__request = request
 
         def get_current(self) -> Optional[rules.User]:
-            return optionally(sculptures.user_of)(
+            return optionally(sculptures.rules.user_of)(
                 models.User.objects.filter(id=self.__request.user.id).first()
             )
 
@@ -75,14 +41,11 @@ class django_orm_case_tasks:
         def top_map_of(id: int) -> Optional[rules.TopMap]:
             top = models.MapTop.objects.filter(id=id).first()
 
-            return optionally(sculptures.top_map_of)(top)
+            return optionally(sculptures.rules.top_map_of)(top)
 
     @val
     class tasks:
-        def saved[S: type(_sculpture_original=models.Task)](
-            task_sculpture: S,
-        ) -> S:
-            task_record = original_of(task_sculpture)
-            task_record.save()
-
-            return task_sculpture
+        def saved(task: rules.Task) -> rules.Task:
+            return sculptures.rules.task_of(
+                sculptures.DjangoOrmRecords(are_saved=True).task_of(task)
+            )
--- apps/map/adapters/sculptures.py
+++ apps/map/adapters/sculptures.py
@@ -1,10 +1,11 @@
-from typing import Iterator, Callable
+from functools import wraps
+from typing import Iterator, Callable, Iterable, Self
 
-from act import optionally, Unia, type, val, out
-from django.db.models.query import QuerySet
+from act import optionally, obj, type, val, partially, Unia, out, by, Pm, Cn
+from django import db
 
 from apps.map import models
-from apps.map.core import rules
+from apps.map import core
 
 
 class QuerySetSculpture[OriginalT, RuleT, OwnerT]:
@@ -13,7 +14,7 @@ class QuerySetSculpture[OriginalT, RuleT, OwnerT]:
     def __init__(
         self,
         for_rules: Callable[OriginalT, RuleT],
-        query_set: "QuerySet[OriginalT, OriginalT]",
+        query_set: "db.models.query.QuerySet[OriginalT, OriginalT]",
         owner: OwnerT,
     ) -> None:
         self.query_set = query_set
@@ -24,88 +25,247 @@ class QuerySetSculpture[OriginalT, RuleT, OwnerT]:
         return map(self.__for_rules, self.query_set)
 
 
-def _to_make_sculpture[V, R](make: Callable[V, R]) -> Callable[
-    V,
-    Unia[R, type(_sculpture_original=V)]
-]:
-    def make_sculpture(value):
-        result = make(value)
-
-        return val(result) & val(_sculpture_original=value)
-
-    return make_sculpture
+type _DjangoOrmRecord = db.models.Model
 
 
-@_to_make_sculpture
-def task_settings_of(
-    task_settings_record: models.TaskSettings,
-) -> rules.TaskSettings:
-    return rules.TaskSettings(
-        remove_task_on=rules.TaskStatus(task_settings_record.remove_task_on),
-    )
+@partially
+def _with_original_which_is[O, V](
+    original: O,
+    value: V,
+) -> Unia[V, type(_sculpture_original=O)]:
+    return val(value) & type(_sculpture_original=original)
 
 
-@_to_make_sculpture
-def submap_of(map_record: models.Map) -> rules.Submap:
-    global_task_settings = optionally(task_settings_of)(
-        map_record.global_task_settings,
-    )
+@obj
+class rules:
+    def task_settings_of(
+        self,
+        task_settings_record: models.TaskSettings,
+    ) -> core.rules.TaskSettings:
+        remove_task_on = rules.TaskStatus(task_settings_record.remove_task_on)
 
-    return rules.Submap(
-        id=map_record.id,
-        user_position=rules.Position(x=map_record.x, y=map_record.y),
-        global_task_settings=global_task_settings,
-        tasks=QuerySetSculpture(task_of, map_record.tasks.all(), map_record),
-    )
+        return _with_original_which_is(task_settings_record)(rules.TaskSettings(
+            remove_task_on=remove_task_on,
+        ))
+
+    def submap_of(self, map_record: models.Map) -> core.rules.Submap:
+        global_task_settings = optionally(rules.task_settings_of)(
+            map_record.global_task_settings,
+        )
+
+        tasks = QuerySetSculpture(
+            self.task_of,
+            map_record.tasks.all(),
+            map_record,
+        )
+
+        return _with_original_which_is(map_record)(rules.Submap(
+            id=map_record.id,
+            user_position=rules.Position(x=map_record.x, y=map_record.y),
+            global_task_settings=global_task_settings,
+            tasks=tasks,
+        ))
+
+    def task_of(self, task_record: models.Task) -> core.rules.Task:
+        return _with_original_which_is(task_record)(core.rules.Task(
+            id=task_record.id,
+            description=task_record.description,
+            status=rules.TaskStatus(task_record.status),
+            settings=optionally(self.task_settings_of)(task_record.settings),
+            submap=optionally(self.submap_of)(task_record),
+            position=rules.Position(x=task_record.x, y=task_record.y),
+        ))
+
+    def top_map_of(self, map_top_record: models.MapTop) -> core.rules.TopMap:
+        map_ = map_top_record.map
+
+        global_task_settings = optionally(self.task_settings_of)(
+            map_.global_task_settings,
+        )
+
+        user_position = rules.Position(x=map_.x, y=map_.y)
+
+        tasks = QuerySetSculpture(
+            self.task_of,
+            map_.tasks.all(),
+            map_top_record,
+        )
+
+        return _with_original_which_is(map_top_record)(rules.TopMap(
+            id=map_top_record.id,
+            name=map_top_record.name,
+            global_task_settings=global_task_settings,
+            user_position=user_position,
+            tasks=tasks,
+            next=optionally(self.top_map_of)(map_top_record.next),
+            previous=optionally(self.top_map_of)(out(map_top_record).previous),
+        ))
+
+    def user_of(self, user_record: models.User) -> core.rules.User:
+        global_task_settings = optionally(self.task_settings_of)(
+            user_record.global_task_settings,
+        )
+
+        maps = QuerySetSculpture(
+            self.top_map_of,
+            user_record.maps.all(),
+            user_record,
+        )
+
+        return _with_original_which_is(user_record)(rules.User(
+            id=user_record.id,
+            maps=maps,
+            global_task_settings=global_task_settings,
+        ))
 
 
-@_to_make_sculpture
-def task_of(task_record: models.Task) -> rules.Task:
-    return rules.Task(
-        id=task_record.id,
-        description=task_record.description,
-        status=rules.TaskStatus(task_record.status),
-        settings=optionally(task_settings_of)(task_record.settings),
-        submap=optionally(submap_of)(task_record),
-        position=rules.Position(x=task_record.x, y=task_record.y),
-    )
+class DjangoOrmRecords:
+    def __init__(self, *, are_saved: bool = False) -> None:
+        self._are_saved = are_saved
 
+    @staticmethod
+    def __lazy_record_converter_method_decorator[
+        S: Self,
+        V,
+        R: _DjangoOrmRecord,
+    ](
+        record_of: Callable[Cn[S, V, Pm], R]
+    ) -> Callable[Cn[S, V, Pm], R]:
+        @wraps(record_of)
+        def decorated_converter(
+            self: S,
+            value: V,
+            *args: Pm.args,
+            **kwargs: Pm.kwargs,
+        ) -> R:
+            if not self._are_saved:
+                original = out(value)._sculpture_original
 
-@_to_make_sculpture
-def top_map_of(map_top_record: models.MapTop) -> rules.TopMap:
-    map_ = out(map_top_record).map
+                if isinstance(original, _DjangoOrmRecord):
+                    return original
 
-    global_task_settings = optionally(task_settings_of)(
-        out(map_).global_task_settings,
-    )
+            return record_of(self, value, *args, **kwargs)
 
-    user_position = rules.Position(x=map_.x, y=map_.y)
+        return decorated_converter
 
-    tasks = QuerySetSculpture(
-        task_of,
-        map_.tasks.all(),
-        map_top_record,
-    )
+    @__lazy_record_converter_method_decorator
+    def task_settings_of(
+        self,
+        task_settings: core.rules.TaskSettings,
+    ) -> models.TaskSettings:
+        return self._current(models.TaskSettings(
+            remove_task_on=task_settings.remove_task_on.value,
+        ))
 
-    return rules.TopMap(
-        id=map_top_record.id,
-        name=map_top_record.name,
-        global_task_settings=global_task_settings,
-        user_position=user_position,
-        tasks=tasks,
-        next=optionally(top_map_of)(map_top_record.next),
-        previous=optionally(top_map_of)(out(map_top_record).previous),
-    )
+    @__lazy_record_converter_method_decorator
+    def user_of(self, user: core.rules.User) -> models.User:
+        settings_record = self.task_settings_of(user.global_task_settings)
 
+        user_record = self._current(models.User(
+            id=user.id,
+            global_task_settings=settings_record,
+        ))
 
-@_to_make_sculpture
-def user_of(user_record: models.User) -> rules.User:
-    global_task_settings = optionally(task_settings_of)(
-        user_record.global_task_settings,
-    )
+        self.__map_tops_of(user.maps, user_record)
 
-    return rules.User(
-        id=user_record.id,
-        maps=QuerySetSculpture(top_map_of, user_record.maps.all(), user_record),
-        global_task_settings=global_task_settings,
-    )
+        return user_record
+
+    @__lazy_record_converter_method_decorator
+    def map_top_of(
+        self,
+        top_map: core.rules.TopMap,
+        user_record: models.User,
+    ) -> models.MapTop:
+        map_record = self.map_of(top_map)
+
+        map_top_of = optionally(self.map_top_of |by| user_record)
+
+        return self._current(models.MapTop(
+            id=top_map.id,
+            map=map_record,
+            user=user_record,
+            name=top_map.name,
+            next=map_top_of(map_record.next),
+            previous=map_top_of(out(map_record).previous),
+        ))
+
+    @__lazy_record_converter_method_decorator
+    def map_of(self, top_map: core.rules.TopMap) -> models.Map:
+        task_settings_of = optionally(self.task_settings_of)
+        settings_record = task_settings_of(top_map.global_task_settings)
+
+        map_record = self._current(models.Map(
+            global_task_settings=settings_record,
+            x=top_map.user_position.x,
+            y=top_map.user_position.y,
+        ))
+
+        self.__tasks_of(top_map.tasks, map_record)
+
+        return map_record
+
+    @__lazy_record_converter_method_decorator
+    def task_of(
+        self,
+        task: core.rules.Task,
+        root_map_record: models.Map,
+    ) -> models.Task:
+        return self._current(models.Task(
+            id=task.id,
+            root_map=root_map_record,
+            description=task.description,
+            status=task.status.value,
+            settings=optionally(self.task_settings_of)(task.settings),
+            x=task.position.x,
+            y=task.position.y,
+            submap=optionally(self.submap_of)(task.submap)
+        ))
+
+    @__lazy_record_converter_method_decorator
+    def submap_of(self, submap: core.rules.Submap) -> models.Map:
+        submap_record = self._current(models.Map(
+            id=submap.id,
+            global_task_settings=optionally(self.task_settings_of)(
+                submap.global_task_settings
+            ),
+            x=submap.user_position.x,
+            y=submap.user_position.y,
+        ))
+
+        self.__tasks_of(submap.tasks, submap_record)
+
+        return submap_record
+
+    @partially
+    def _current[R: _DjangoOrmRecord](self, record: R) -> R:
+        if self._are_saved:
+            record.save()
+
+        return record
+
+    def __records_of[V, R: _DjangoOrmRecord](
+        self,
+        values: Iterable[V],
+        *,
+        record_of: Callable[V, R],
+    ) -> tuple[R, ...]:
+        if self._are_saved and not isinstance(values, QuerySetSculpture):
+            return tuple(record_of(value) for value in values)
+
+        return tuple()
+
+    def __map_tops_of(
+        self,
+        maps: Iterable[core.rules.TopMap],
+        user_record: models.User
+    ) -> tuple[models.MapTop, ...]:
+        record_of = self.map_top_of |by| user_record
+
+        return self.__records_of(maps, record_of=record_of)
+
+    def __tasks_of(
+        self,
+        tasks: Iterable[core.rules.Task],
+        map_record: models.Map,
+    ) -> tuple[models.Task, ...]:
+        return self.__records_of(tasks, record_of=self.task_of |by| map_record)
